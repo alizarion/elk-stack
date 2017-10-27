b25243feb13b272c4a9f9cbcd5ebd7f0beb73518
Selim BENSENOUCI <selim@openlinux.fr>
2017-10-27 15:04:05 +0200

    adding custom nginx  naxsi

--- proxy/conf/opt/entry-point.sh
+++ proxy/conf/opt/entry-point.sh
@@ -21,6 +21,35 @@ if [ x${LEARNING_MODE} != x"yes" ]
 	echo "LearningMode is enabled"
 fi
 
+if [ ! -z "$SERVER_NAME" ]
+    then
+	echo "Server name :"
+	echo $SERVER_NAME
+    else
+    export SERVER_NAME="localhost"
+	echo "Setting server name as default value : localhost"
+fi
+sed -i  -e "s;%SERVER_NAME%;${SERVER_NAME};g"  /etc/nginx/sites-enabled/default
+
+if [ x${SSL_MODE} != x"prod" ]
+	then
+	echo "SSL mode enabled"
+	else
+	echo "SSL mode TEST, self signed certificate will be generated"
+fi
+
+if [ x${FORCE_SSl} != x"yes" ]
+	then
+	echo "SSL redirect enabled"
+	sed -i  -e "s,%FORCE_SSl%,if (\$scheme = http) { return 301 https://\$server_name\$request_uri;  break; },g"  /etc/nginx/sites-enabled/default
+	else
+    sed -i  -e "s;%FORCE_SSl%;;g"  /etc/nginx/sites-enabled/default
+
+	echo "SSL mode TEST, self signed certificate will be generated"
+fi
+
+
+
 echo $@
 
 exec "$@"
